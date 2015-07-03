c43f692070e1181d637bc0181e6c400be6d552e1
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-07-03 18:43:01 -0300

    Paso 1

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,14 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
+(function(){
+angular.module('App', ['ionic'])
+.config(function($stateProvider,$urlRouterProvider) {
+$stateProvider
+    .state('feed', {
+      url: '/feed',
+      templateUrl: 'views/app/feed.html',
+    })
+    ;
+$urlRouterProvider.otherwise("/feed");
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -17,3 +21,4 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+}());
